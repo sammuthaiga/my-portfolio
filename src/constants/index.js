b9dcdbae987d746162a764a88c1e890bb572211b
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-05-22 23:44:53 +0300

    feat: add the testimonials section

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,7 +23,7 @@ import {
     jobit,
     tripguide,
     threejs,
-    // project3,
+    //  project3,
   } from "../assets";
   
   export const navLinks = [
@@ -174,25 +174,25 @@ import {
     {
       testimonial:
         "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
+      name: "Lucy Wamburia",
       designation: "CFO",
-      company: "Acme Co",
+      company: "City Orchard",
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
         "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
+      name: "Jeff N.",
+      designation: "Founder",
+      company: "FeelFittness",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
+        "After Sam optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Liz D Mukami",
       designation: "CTO",
-      company: "456 Enterprises",
+      company: "Movers",
       image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
   ];
@@ -201,7 +201,7 @@ import {
     {
       name: "Movers App",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "Web-based platform that allows users to search, book, and manage their moves from various locations, providing a convenient and efficient solution for transportation needs and labour.",
       tags: [
         {
           name: "react",
@@ -209,7 +209,7 @@ import {
         },
         {
           name: "Ruby on Rails",
-          color: "red-text-gradient",
+          color: "green-text-gradient",
         },
         {
           name: "tailwind",
@@ -217,12 +217,12 @@ import {
         },
       ],
       image: carrent,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/sammuthaiga/movers-ui",
     },
     {
-      name: "Job IT",
+      name: "City Orchard",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "Web application that enables users to search for fresh fruits, view estimated prices and farmers contacts, and locate their current location for fast delivery.",
       tags: [
         {
           name: "react",
@@ -233,33 +233,33 @@ import {
           color: "green-text-gradient",
         },
         {
-          name: "scss",
+          name: "Ruby on Rails",
           color: "pink-text-gradient",
         },
       ],
       image: jobit,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/sammuthaiga/City-orchard-front-end",
     },
     {
-      name: "Trip Guide",
+      name: "Flexy Gym",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "A comprehensive gym booking platform that allows users to book workout sessions, book a trainer, and buy work out costumes. The app offers curated recommendations for popular destinations around your home area..",
       tags: [
         {
-          name: "nextjs",
+          name: "React js",
           color: "blue-text-gradient",
         },
         {
-          name: "supabase",
+          name: "javascript",
           color: "green-text-gradient",
         },
         {
-          name: "css",
+          name: "Tailwind",
           color: "pink-text-gradient",
         },
       ],
       image: tripguide,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/sammuthaiga/Fitness-App",
     },
   ];
   
